647d9140ecf5cad4baffb2caf2a230925ce3e665
Topper <Topper@somewhere.net>
2024-08-23 05:58:18 -0400

    Update images and main page appearance

--- components/karlsen-qrcode.tsx
+++ components/karlsen-qrcode.tsx
@@ -4,16 +4,16 @@ export default function KarlsenQrCode(props) {
     return (
         <QRCode
             value={props.value}
-            fgColor='#134a40'
+            fgColor='#1C3F5E'
             eyeRadius={[
                 [10, 10, 0, 10], // top/left eye
                 [10, 10, 10, 0], // top/right eye
                 [10, 0, 10, 10], // bottom/left
             ]}
             eyeColor={[
-                { outer: '#134a40', inner: '#49EACB' },
-                { outer: '#134a40', inner: '#49EACB' },
-                { outer: '#134a40', inner: '#49EACB' },
+                { outer: '#1C3F5E', inner: '#38B6FF' },
+                { outer: '#1C3F5E', inner: '#38B6FF' },
+                { outer: '#1C3F5E', inner: '#38B6FF' },
             ]}
         />
     );
