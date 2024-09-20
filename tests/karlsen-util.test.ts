d226996322b5ee202527dfa8d6753e2ddb47eaf8
Lemois 1337 <lemois1337@protonmail.com>
2024-09-20 15:31:15 +0200

    Pass all tests with Karlsen

--- tests/karlsen-util.test.ts
+++ tests/karlsen-util.test.ts
@@ -6,7 +6,7 @@ import {
 
 describe('addressToPublicKey', () => {
     it('should be able to round-trip', () => {
-        const testAddr = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35saatu88xq';
+        const testAddr = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35sh2ztf2xt';
         const pubkey = addressToPublicKey(testAddr);
         const roundtripPublicKey = publicKeyToAddress(Buffer.from(pubkey.publicKey), false);
 
@@ -19,7 +19,7 @@ describe('addressToPublicKey', () => {
             'hex',
         );
         const expectedVersion = 0;
-        const address = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35saatu88xq';
+        const address = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35sh2ztf2xt';
 
         const result = addressToPublicKey(address);
         expect(Buffer.from(result.publicKey)).toStrictEqual(expectedPublicKey);
@@ -32,7 +32,7 @@ describe('addressToPublicKey', () => {
             'hex',
         );
         const expectedVersion = 8;
-        const address = 'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jgxfzfc98';
+        const address = 'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jz3q4849v';
 
         const result = addressToPublicKey(address);
         expect(Buffer.from(result.publicKey)).toStrictEqual(expectedPublicKey);
@@ -45,7 +45,7 @@ describe('addressToPublicKey', () => {
             'hex',
         );
         const expectedVersion = 1;
-        const address = 'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s7fd82n0v';
+        const address = 'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s39wz94ap';
 
         const result = addressToPublicKey(address);
         expect(Buffer.from(result.publicKey)).toStrictEqual(expectedPublicKey);
@@ -56,7 +56,7 @@ describe('addressToPublicKey', () => {
 describe('publicKeyToAddress', () => {
     it('should be able to convert to a schnorr-based address', () => {
         const expectedAddress =
-            'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35saatu88xq';
+            'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35sh2ztf2xt';
         const publicKey = Buffer.from(
             '21eb0c4270128b16c93c5f0dac48d56051a6237dae997b58912695052818e348',
             'hex',
@@ -68,7 +68,7 @@ describe('publicKeyToAddress', () => {
     });
 
     it('should be able to convert to a schnorr-based address without prefix', () => {
-        const expectedAddress = 'qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35saatu88xq';
+        const expectedAddress = 'qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35sh2ztf2xt';
         const publicKey = Buffer.from(
             '21eb0c4270128b16c93c5f0dac48d56051a6237dae997b58912695052818e348',
             'hex',
@@ -79,7 +79,7 @@ describe('publicKeyToAddress', () => {
 
     it('should be able to convert to a p2sh address', () => {
         const expectedAddress =
-            'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jgxfzfc98';
+            'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jz3q4849v';
         const publicKey = Buffer.from(
             'f38031f61ca23d70844f63a477d07f0b2c2decab907c2e096e548b0e08721c79',
             'hex',
@@ -90,7 +90,7 @@ describe('publicKeyToAddress', () => {
 
     it('should be able to convert to a ECDSA-based address', () => {
         const expectedAddress =
-            'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s7fd82n0v';
+            'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s39wz94ap';
         const publicKey = Buffer.from(
             '02d5fdc7ad11a65d0bbe7882fc3dbc91b5861d182dcce79f7c1be5bfd30a677cd6',
             'hex',
@@ -104,7 +104,7 @@ describe('addressToScriptPublicKey', () => {
     it('should be able to convert a schnorr address into script public key', () => {
         const expectedScriptPublicKey =
             '2021eb0c4270128b16c93c5f0dac48d56051a6237dae997b58912695052818e348ac';
-        const address = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35saatu88xq';
+        const address = 'karlsen:qqs7krzzwqfgk9kf830smtzg64s9rf3r0khfj76cjynf2pfgrr35sh2ztf2xt';
 
         const result = addressToScriptPublicKey(address);
         expect(result).toBe(expectedScriptPublicKey);
@@ -113,7 +113,7 @@ describe('addressToScriptPublicKey', () => {
     it('should be able to convert an ecdsa address into script public key', () => {
         const expectedScriptPublicKey =
             '2102d5fdc7ad11a65d0bbe7882fc3dbc91b5861d182dcce79f7c1be5bfd30a677cd6ab';
-        const address = 'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s7fd82n0v';
+        const address = 'karlsen:qypdtlw845g6vhgtheug9lpahjgmtpsarqkueeul0sd7t07npfnhe4s39wz94ap';
 
         const result = addressToScriptPublicKey(address);
         expect(result).toBe(expectedScriptPublicKey);
@@ -122,7 +122,7 @@ describe('addressToScriptPublicKey', () => {
     it('should be able to convert a p2sh address into script public key', () => {
         const expectedScriptPublicKey =
             'aa20f38031f61ca23d70844f63a477d07f0b2c2decab907c2e096e548b0e08721c7987';
-        const address = 'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jgxfzfc98';
+        const address = 'karlsen:precqv0krj3r6uyyfa36ga7s0u9jct0v4wg8ctsfde2gkrsgwgw8jz3q4849v';
 
         const result = addressToScriptPublicKey(address);
         expect(result).toBe(expectedScriptPublicKey);
